f2f8fc75da2d7c7d041584800336c7a5a45b471e
Erick Fernando Martins Santos <erickmartinslima3@gmail.com>
2026-06-02 15:08:02 -0300

    fix: codigo para instalar account

--- l10n_br_fiscal/__manifest__.py
+++ l10n_br_fiscal/__manifest__.py
@@ -58,7 +58,7 @@
         "data/l10n_br_fiscal.nbm.csv",
         "data/l10n_br_fiscal.nbs.csv",
         "data/l10n_br_fiscal.cest.csv",
-        "data/l10n_br_fiscal_icms_tax_definition_data.xml",
+        # "data/l10n_br_fiscal_icms_tax_definition_data.xml",
         # Views
         "views/tax_classification.xml",
         "views/legal_nature_view.xml",
